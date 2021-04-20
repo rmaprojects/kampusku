4192f09d32aa6e276e27b50e8efa71bffb35b82f
RMA-17 <rakamuhammadalhafidz12312@gmail.com>
2021-04-20 11:11:01 +0700

    Update List Siswa

--- list_siswa.php
+++ list_siswa.php
@@ -1,7 +1,9 @@
 <?php
+
     include 'connection.php';
 
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
         $jumlah_siswa = "SELECT COUNT(*) 'total' FROM tbl_siswa";
         $execute_jumlah_siswa = mysqli_query($_AUTH, $jumlah_siswa);
 
@@ -12,30 +14,32 @@
         $executemencariwali = mysqli_query($_AUTH, $wali_kelas);
 
         $mencari_datasiswa = "SELECT tbl_siswa.nama_siswa, tbl_siswa.jenis_kelamin, tbl_siswa.alamat, tbl_siswa.email, tbl_siswa.tanggal_terdaftar FROM tbl_siswa";
-
-        $response["message"] = trim("Test Data berhasil di tarik dari database");
+        
+        $response["message"] = "Data dari kelas X berhasil ditampilkan";
         $response["code"] = 201;
-        $response["status"] = true;
+        $response["status"] = false;
         $response['total_datasiswa'] = $execute_jumlah_siswa;
         $response['wali_kelas'] = $executemencariwali;
         $response['datasiswa'] = array();
 
-        while ($row = mysqli_fetch_array($execute_informasiesemka)) {
+        while ($row = mysqli_fetch_array($mencari_datasiswa)){
             $data = array();
 
-            $data["nomorabsen"] = $row["nomorabsen"];
-            $data["namalengkap"] = $row["namalengkap"];
-            $data["umur"] = $row["umur"];
-            $data["tahunlahir"] = $row["tahunlahir"];
-
-            array_push($response["testdata"], $data);
+            $data["nama_lengkap"] = $row["nama_lengkap"];
+            $data["jenis_kelamin"] = $row["jenis_kelamin"];
+            $data["alamat"] = $row["alamat"];
+            $data["email"] = $row["email"];
+            $data["tanggal_terdaftar"] = $row["tanggal_terdaftar"];
+            
+            array_push($response['datasiswa'], $data);
         }
         echo json_encode($response);
     } else {
-        $_response["message"] = trim("Oops! Sory, Request API ini membutuhkan parameter!.");
-        $_response["code"] = 400;
-        $_response["status"] = false;
+        $response['message'] = "API ini memerlukan parameter";
+        $response['code'] = 400;
+        $response['status'] = false;
 
         echo json_encode($response);
     }
+
 ?>
