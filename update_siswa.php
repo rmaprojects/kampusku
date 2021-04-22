e993d93f3ec50b538b5a4609fac4f20243a334d7
RMA-17 <rakamuhammadalhafidz12312@gmail.com>
2021-04-22 07:12:37 +0700

    Add update_siswa & screenshotnya

--- update_siswa.php
+++ update_siswa.php
@@ -10,64 +10,55 @@
         $alamat = $_POST['alamat'];
         $nomor_telp = $_POST['nomor_telp'];
         $email = $_POST['email'];
+        $id_jurusan = $_POST['id_jurusan'];
+        $kelas = $_POST['id_kelas'];
 
         $cek_data = "SELECT * FROM tbl_siswa WHERE nis = '$nis'";
         $check_ifexist = mysqli_fetch_array(mysqli_query($_AUTH, $cek_data));
 
-        if (isset($check_ifexist)){
+        if (isset($check_ifexist)) {
+            $query_update_datasiswa = "UPDATE `tbl_datasiswa` SET `id_kelas` = '$kelas', `id_jurusan` = '$id_jurusan' WHERE `tbl_datasiswa`.`nis` = '$nis'";
+            if (mysqli_query($_AUTH, $query_update_datasiswa)){
+                $query_update_siswa = "UPDATE `tbl_siswa` SET `nama_siswa` = '$nama_siswa', `jenis_kelamin` = '$jenis_kelamin', `alamat` = '$alamat', `nomor_telp` = '$nomor_telp', `email` = '$email' WHERE `tbl_siswa`.`nis` = '$nis'";
+                $exe_update_siswa = mysqli_query($_AUTH, $query_update_siswa);
 
-            $query_update = "UPDATE tbl_siswa SET tbl_siswa.nama_siswa = '$nama_siswa', tbl_siswa.jenis_kelamin = '$jenis_kelamin', tbl_siswa.alamat = '$alamat', tbl_siswa.nomor_telp = '$nomor_telp', tbl_siswa.email = '$email' WHERE tbl_siswa.nis = '$nis'";
-            
-            if (mysqli_query($query_update)){
-                $jurusan = $_POST['id_jurusan'];
-                $kelas = $_POST['id_kelas'];
+                $query_search_data = "SELECT * FROM tbl_siswa WHERE nis ='$nis'";
+                $exe_data_add = mysqli_query($_AUTH, $query_search_data);
 
-                $query_update_datasiswa = "UPDATE tbl_datasiswa SET tbl_datasiswa.id_kelas = $kelas, tbl_datasiswa.id_jurusan = '$jurusan' WHERE tbl_datasiswa.nis = '0128392'";
-                if (mysqli_query($query_update_datasiswa)){
+                $response['message'] = trim("Siswa dengan nama $nama_siswa telah diupdate");
+                $response['code'] = 200;
+                $response['status'] = true;
+                $response['update_siswa'] = array();
+                while ($row = mysqli_fetch_array($exe_data_add)){
+                    $data = array();
 
-                    $query_search_data = "SELECT * FROM tbl_siswa WHERE nis ='$nis'";
-                    $exe_data_add = mysqli_query($_AUTH, $query_search_data);
-
-                    $response['message'] = "Data dari Siswa bernama $nama_siswa sudah diupdate";
-                    $response['code'] = "201";
-                    $response['status'] = true;
-                    $response['detailupdate'] = array();
-                    while ($row = mysqli_fetch_array($exe_data_add)){
-                        $data = array();
-
-                        $data['nama_siswa'] = $row ['nama_siswa'];
-                        $data['jenis_kelamin'] = $row ['jenis_kelamin'];
-                        $data['alamat'] = $row ['alamat'];
-                        $data['nomor_telp'] = $row ['nomor_telp'];
-                        $data['email'] = $row ['email'];
-                        $data['tanggal_terdaftar'] = $row['tanggal_terdaftar'];
-                        
-                        array_push($_response['detailupdate'], $data);
-                    }
-                    echo json_encode($_response);
+                    $data['nama_siswa'] = $row ['nama_siswa'];
+                    $data['jenis_kelamin'] = $row ['jenis_kelamin'];
+                    $data['alamat'] = $row ['alamat'];
+                    $data['nomor_telp'] = $row ['nomor_telp'];
+                    $data['email'] = $row ['email'];
                     
+                    array_push($response['update_siswa'], $data);
                 }
+                echo json_encode($response);
             } else {
-                $response['message'] = "Ada sesuatu yang salah, silahkan dicoba ulang";
-                $response['code'] = 500;
+                $response['message'] = "GAGAL";
+                $response['code'] = 400;
                 $response['status'] = false;
+                echo json_encode($response);
             }
         } else {
-            $response['message'] = "Data tidak ditemukan";
-            $response['code'] = 404;
-            $response['message'] = false;
+            $response['message'] = "NIS tidak ditemukan, coba ulang";
+            $response['code'] = 403;
+            $response['status'] = false;
+            echo json_encode($response);
         }
+
     } else {
         $response['message'] = "Need an API";
-        $response['code'] = 401;
+        $response['code'] = 400;
         $response['status'] = false;
+        echo json_encode($response);
     }
-
-
-
-
-
-
-
 
 ?>
